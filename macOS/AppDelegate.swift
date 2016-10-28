4b9d0329e359a68fce8f03729d3e0081c97c7d5b
Pablo Henrique Bertaco <pablo_fonseca91@icloud.com>
2016-10-28 11:12:47 -0200

    Initial Commit

---
+++ macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  GameV
+//
+//  Created by Pablo Henrique Bertaco on 10/28/16.
+//  Copyright © 2016 PabloHenri91. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
